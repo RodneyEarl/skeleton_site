72de0ad79ea72ba0b60a0576b6d923118159215d
Rodney Earl <rsearl.90@gmail.com>
2013-10-11 22:32:06 -0700

    Firs commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkeletonSite::Application.config.secret_key_base = 'a3f2c2dc98ea19f8cf188a7066055b1f6a52a41a43bcad0062691963f9c1ffcb70f26d16c0c9d625c2c04358fc923d678bedf9084c15ffcf2abc31e9477c8446'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkeletonSite::Application.config.session_store :cookie_store, key: '_skeleton_site_session'
